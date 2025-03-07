79871506662e4feed4a60772fc777013e88eb645
Fábio Carvalho <84592352+fabioacarvalho@users.noreply.github.com>
2025-03-07 22:54:43 +0000

    up

--- pages/Face_Recognition.py
+++ pages/Face_Recognition.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from streamlit_cropper import st_cropper
 from PIL import Image
-import cv2
-from skimage import io, color
-from IPython.display import Image
-import os
+import face_recognition
+import PIL.Image
+import PIL.ImageDraw
 
 st.title("Face Recognition")
 st.caption("Developed by Fábio A. Carvalho, 2025")
@@ -13,8 +12,14 @@ st.caption("Developed by Fábio A. Carvalho, 2025")
 img_file = st.file_uploader("Select an image", type=["png", "jpg", "jpeg"])
 
 if img_file is not None:
-    st.toast("Image loaded with success.", icon=":material/thumb_up:")
-    img = io.imread(img_file)
-    st.caption(img.shape)
+    img = face_recognition.load_image_file(img_file)
+    faces_found = face_recognition.face_locations(img)
 
+    image_array = PIL.Image.fromarray(img)
+    for face in faces_found:
+        top, left, bottom, right = face
+        draw = PIL.ImageDraw.Draw(image_array)
+        draw.rectangle([left, top, right, bottom], outline="green", width=10)
 
+    st.image(image_array)
+    st.toast("Imagem carregada com sucesso!", icon=":material/thumb_up:")
